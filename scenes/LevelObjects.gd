1c709fed052e40b7fc055df62b84faab2d4075e7
Thomas <mutualtape@gmail.com>
2024-04-18 15:07:04 +0200

    removed ugly stripes of level image by using child clipping instead of filling another polygon

--- scenes/LevelObjects.gd
+++ scenes/LevelObjects.gd
@@ -1,12 +1,24 @@
 extends StaticBody2D
 
+"""
+For all sprites - three states: transparent (alpha=100%), lucid: >0%, solid (0%)
+
+Sprite gets painted as is, it is if a tile is applied 
+- Sprite (image), clip children = clip only
+-- Sprite with tile as texture, region with inf width and height
+
+Collision: solid
+LightOccluder: solid and lucid (lucid: todo)
+
+"""
+
+@export var tiled_texture: Texture = null
+
 func _ready():
 	
 	for sprite in get_children():
-	
 		if ! sprite is Sprite2D: 
 			continue
-		sprite.visible = false
 	
 		var bitMap = BitMap.new()	
 		bitMap.create_from_image_alpha(sprite.texture.get_image())
@@ -17,6 +29,17 @@ func _ready():
 	
 		var imgpols = bitMap.opaque_to_polygons(Rect2(Vector2(), bitMap.get_size()), 2)
 		
+		if(tiled_texture != null):
+			sprite.clip_children = CanvasItem.CLIP_CHILDREN_ONLY
+			var rock: Sprite2D = Sprite2D.new()
+			rock.texture = tiled_texture
+			rock.texture_repeat = CanvasItem.TEXTURE_REPEAT_ENABLED
+			rock.region_enabled = true
+			rock.region_rect = Rect2(0,0,100000,100000)
+			rock.light_mask = 2 # matches drone's outer light cull mask
+			rock.modulate = Color(0.2,0.2,0.2)
+			sprite.add_child(rock)
+		
 		for imgpol in imgpols:
 			var pol = PackedVector2Array()
 			for a in imgpol:
@@ -25,15 +48,11 @@ func _ready():
 			
 			var collision = CollisionPolygon2D.new()
 			collision.polygon = pol;
-			
-			var rock = $Rock.duplicate()
-			rock.polygon = pol
-			add_child(rock)
+			add_child(collision)
 			
 			var lightOccluder: LightOccluder2D = $LightOccluder2D.duplicate()
 			lightOccluder.occluder = OccluderPolygon2D.new()
 			lightOccluder.occluder.polygon = pol
-			
 			add_child(lightOccluder)
+				
 			
-			add_child(collision)
